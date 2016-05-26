1b5bdd60b3aa15c42784d0af8ef7a89e8e9e837f
Alex Poliakov <apoliakov@paradigm4.com>
2016-05-26 16:33:21 -0400

    Added sum client

---
+++ src/R_sum.R
@@ -0,0 +1,32 @@
+# invoke with, for example:
+# iquery -aq "stream(build(<val:double> [i=1:5,5,0], i), 'Rscript /home/blewis/streaming/src/R_script.R', 'format=df', 'types=double,int32')"
+
+con_in = file("stdin", "rb")
+con_out = pipe("cat", "wb")
+total = NA
+while( TRUE )
+{
+  input_list = unserialize(con_in)
+  ncol = length(input_list)
+  if(ncol == 0) #this is the last message
+  {
+    if(is.na(total))
+    {
+      writeBin(serialize(list(), NULL, xdr=FALSE), con_out)
+    }
+    else
+    {
+      writeBin(serialize(list(s=total), NULL, xdr=FALSE), con_out)
+    }
+    flush(con_out)
+    break
+  }
+  if(is.na(total))
+  {
+    total = 0
+  }
+  total = total + sum(input_list[[1]])
+  writeBin(serialize(list(), NULL, xdr=FALSE), con_out)
+  flush(con_out)
+}
+close(con_in)
