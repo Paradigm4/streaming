0e8366ecab3bf2b8b468f89cd2bec12c3d153a38
Kriti Sen Sharma <ksen@paradigm4.com>
2016-11-04 21:08:19 +0000

    changed .so name for stream to streaming

--- src/restart.sh
+++ src/restart.sh
@@ -8,11 +8,11 @@ echo "Using scidb at " $SCIDB_BIN
 echo "Using config " $CONFIG_NAME
 
 
-iquery -aq "unload_library('stream')"
+iquery -aq "unload_library('streaming')"
 set -e
 set -x
 make
 scidb.py stopall $CONFIG_NAME
-cp libstream.so $SCIDB_BIN/../lib/scidb/plugins
+cp libstreaming.so $SCIDB_BIN/../lib/scidb/plugins
 scidb.py startall $CONFIG_NAME
-iquery -aq "load_library('stream')"
+iquery -aq "load_library('streaming')"
