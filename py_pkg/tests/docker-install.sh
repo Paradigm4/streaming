9b746ca6077b6b30979aeffa271f7126627d60a4
Rares Vernica <rvernica@gmail.com>
2017-09-27 21:00:20 -0700

    Install dev_tools for SciDB in Docker container

--- py_pkg/tests/docker-install.sh
+++ py_pkg/tests/docker-install.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 46BD98A354BA5235
 
+# Setup Bintray.com/rvernica repository for libarrow-dev
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 46BD98A354BA5235
 echo "deb https://dl.bintray.com/rvernica/deb trusty universe" \
      > /etc/apt/sources.list.d/bintray.list
 
+
+# Install dev_tools requirements and libarrow-dev
 apt-get update
-apt-get install --assume-yes libarrow-dev
+apt-get install --assume-yes  --no-install-recommends \
+        bc                                            \
+        libarrow-dev
+
+
+# Install dev_tools
+SCIDB_DEV_TOOLS=332127216cd6d5791320abafd767e09164cd22e2
+wget --no-verbose --output-document -                                       \
+     https://github.com/Paradigm4/dev_tools/archive/$SCIDB_DEV_TOOLS.tar.gz \
+    |  tar --extract --gzip --directory=/usr/local/src                      \
+    && make --directory=/usr/local/src/dev_tools-$SCIDB_DEV_TOOLS           \
+    && cp /usr/local/src/dev_tools-$SCIDB_DEV_TOOLS/*.so                    \
+          $SCIDB_INSTALL_PATH/lib/scidb/plugins
