1732f3ad072068e8a158c2690b03d8b7bb6f6cc6
Rares Vernica <rvernica@gmail.com>
2018-02-03 14:48:43 -0800

    Run Python tests in Travis
    * move all tests to tests/ directory
    * add scidb-py 16.9.10 dependency
    * make test_low.py compatible with Python 3
    * run python style check and python tests from library root

--- tests/test_low.py
+++ tests/test_low.py
@@ -29,7 +29,7 @@ py_type_map = {
     'int64': 'int',
     'double': 'float',
     'string': 'str',
-    'binary': 'str',
+    'binary': 'bytes',
 }
 
 
@@ -42,7 +42,7 @@ def test_one_chunk(db, scidb_ty, name):
     if scidb_ty == 'binary':
         input_ar = db.input(
             '<x:binary not null>[i=0:2:0:3]',
-            upload_data=numpy.array([str(i) for i in range(3)],
+            upload_data=numpy.array([str(i).encode() for i in range(3)],
                                     dtype='object')).store()
         input = input_ar.name
     else:
@@ -52,16 +52,30 @@ def test_one_chunk(db, scidb_ty, name):
     res = db.iquery("""
         stream(
           {input},
-          'python -u /stream/py_pkg/tests/scripts/one_chunk.py',
+          'python -u /stream/tests/scripts/one_chunk.py',
           'format=feather',
           'types={scidb_ty}'{names})""".format(
               input=input,
               scidb_ty=scidb_ty,
               names='' if name is None else ", 'names={}'".format(name)),
-                    fetch=True)
-    assert(numpy.array_equal(res, numpy.array(
+                    fetch=True,
+                    as_dataframe=False)
+    print(res)
+    print(numpy.array(
         [(0, 0, i, (255, eval('{}({})'.format(
             py_type_map.get(scidb_ty, scidb_ty), i))))
+         for i in range(3)],
+        dtype=[('instance_id', '<i8'),
+               ('chunk_no', '<i8'),
+               ('value_no', '<i8'),
+               ('a0' if name is None else name,
+                [('null', 'u1'),
+                 ('val', np_type_map.get(scidb_ty, scidb_ty))])]))
+
+    assert(numpy.array_equal(res, numpy.array(
+        [(0, 0, i, (255, eval('{}({})'.format(
+            py_type_map.get(scidb_ty, scidb_ty),
+            str(i).encode() if scidb_ty == 'binary' else i))))
          for i in range(3)],
         dtype=[('instance_id', '<i8'),
                ('chunk_no', '<i8'),
@@ -80,7 +94,7 @@ def test_any_chunks(db, scidb_ty, name):
     if scidb_ty == 'binary':
         input_ar = db.input(
             '<x:binary not null>[i=0:9:0:3]',
-            upload_data=numpy.array([str(i) for i in range(10)],
+            upload_data=numpy.array([str(i).encode() for i in range(10)],
                                     dtype='object')).store()
         input = input_ar.name
     else:
@@ -90,18 +104,20 @@ def test_any_chunks(db, scidb_ty, name):
     res = db.iquery("""
         stream(
           {input},
-          'python -u /stream/py_pkg/tests/scripts/any_chunks.py',
+          'python -u /stream/tests/scripts/any_chunks.py',
           'format=feather',
           'types={scidb_ty}'{names})""".format(
               input=input,
               scidb_ty=scidb_ty,
               names='' if name is None else ", 'names={}'".format(name)),
                     fetch=True,
-                    atts_only=True)
+                    atts_only=True,
+                    as_dataframe=False)
     np_name = 'a0' if name is None else name
     assert(numpy.array_equal(res[res[np_name]['val'].argsort()], numpy.array(
         [((255, eval('{}({})'.format(
-            py_type_map.get(scidb_ty, scidb_ty), i))),)
+            py_type_map.get(scidb_ty, scidb_ty),
+            str(i).encode() if scidb_ty == 'binary' else i))),)
          for i in range(10)],
         dtype=[(np_name, [('null', 'u1'),
                           ('val', np_type_map.get(scidb_ty, scidb_ty))])])))
