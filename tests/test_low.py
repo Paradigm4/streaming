b72bcbf627df89410a6c743e1829130bfbadb30f
Rares Vernica <rvernica@gmail.com>
2018-02-04 13:51:21 -0800

    Upgrade to latest Arrow, fix #16

--- tests/test_low.py
+++ tests/test_low.py
@@ -60,17 +60,18 @@ def test_one_chunk(db, scidb_ty, name):
               names='' if name is None else ", 'names={}'".format(name)),
                     fetch=True,
                     as_dataframe=False)
-    assert(numpy.array_equal(res, numpy.array(
-        [(0, 0, i, (255, eval('{}({})'.format(
-            py_type_map.get(scidb_ty, scidb_ty),
-            str(i).encode() if scidb_ty == 'binary' else i))))
-         for i in range(3)],
-        dtype=[('instance_id', '<i8'),
-               ('chunk_no', '<i8'),
-               ('value_no', '<i8'),
-               ('a0' if name is None else name,
-                [('null', 'u1'),
-                 ('val', np_type_map.get(scidb_ty, scidb_ty))])])))
+    assert numpy.array_equal(
+        res, numpy.array(
+            [(0, 0, i, (255, eval('{}({})'.format(
+                py_type_map.get(scidb_ty, scidb_ty),
+                str(i).encode() if scidb_ty == 'binary' else i))))
+             for i in range(3)],
+            dtype=[('instance_id', '<i8'),
+                   ('chunk_no', '<i8'),
+                   ('value_no', '<i8'),
+                   ('a0' if name is None else name,
+                    [('null', 'u1'),
+                     ('val', np_type_map.get(scidb_ty, scidb_ty))])]))
 
 
 @pytest.mark.parametrize(('scidb_ty', 'name'), [
@@ -102,13 +103,15 @@ def test_any_chunks(db, scidb_ty, name):
                     atts_only=True,
                     as_dataframe=False)
     np_name = 'a0' if name is None else name
-    assert(numpy.array_equal(res[res[np_name]['val'].argsort()], numpy.array(
-        [((255, eval('{}({})'.format(
-            py_type_map.get(scidb_ty, scidb_ty),
-            str(i).encode() if scidb_ty == 'binary' else i))),)
-         for i in range(10)],
-        dtype=[(np_name, [('null', 'u1'),
-                          ('val', np_type_map.get(scidb_ty, scidb_ty))])])))
+    assert numpy.array_equal(
+        res[res[np_name]['val'].argsort()],
+        numpy.array(
+            [((255, eval('{}({})'.format(
+                py_type_map.get(scidb_ty, scidb_ty),
+                str(i).encode() if scidb_ty == 'binary' else i))),)
+             for i in range(10)],
+            dtype=[(np_name, [('null', 'u1'),
+                              ('val', np_type_map.get(scidb_ty, scidb_ty))])]))
 
 
 def test_arrow_1676(db):
