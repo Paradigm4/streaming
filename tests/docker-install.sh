3da0c55aa63372d1b0d1f20777b485ab37b5d233
Rares Vernica <rvernica@gmail.com>
2018-06-24 14:33:46 -0700

    Skip installing arrow - already installed in the image

--- tests/docker-install.sh
+++ tests/docker-install.sh
@@ -4,19 +4,19 @@ set -o errexit
 
 
 # Install prerequisites
-## https://github.com/red-data-tools/packages.red-data-tools.org#ubuntu
-## No packages for Debian jessie, use Ubuntu trusty
-cat <<APT_LINE | tee /etc/apt/sources.list.d/red-data-tools.list
-deb https://packages.red-data-tools.org/ubuntu/ trusty universe
-APT_LINE
+# ## https://github.com/red-data-tools/packages.red-data-tools.org#ubuntu
+# ## No packages for Debian jessie, use Ubuntu trusty
+# cat <<APT_LINE | tee /etc/apt/sources.list.d/red-data-tools.list
+# deb https://packages.red-data-tools.org/ubuntu/ trusty universe
+# APT_LINE
 
-apt-get update
-apt-get install --assume-yes --no-install-recommends --allow-unauthenticated \
-        red-data-tools-keyring
-apt-get update
-apt-get install --assume-yes --no-install-recommends \
-        libarrow-dev=$ARROW_VER                      \
-        libarrow0=$ARROW_VER
+# apt-get update
+# apt-get install --assume-yes --no-install-recommends --allow-unauthenticated \
+#         red-data-tools-keyring
+# apt-get update
+# apt-get install --assume-yes --no-install-recommends \
+#         libarrow-dev=$ARROW_VER                      \
+#         libarrow0=$ARROW_VER
 apt-get install                                      \
         --assume-yes                                 \
         --no-install-recommends                      \
